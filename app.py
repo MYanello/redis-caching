98dcaa4af0120a4cfc825e6a785b3ea53316ad28
myanello-rescale <myanello@rescale.com>
2023-11-02 15:49:02 -0500

    moving stuff around

--- app.py
+++ app.py
@@ -3,12 +3,29 @@ import time
 import redis
 from cachetools import TTLCache
 import argparse
-### Todo, verify password provided if needed, verify the redis connection. try/except stuff
-def redis_test_conn(r):
+
+parser = argparse.ArgumentParser(description='Redis caching proxy')
+parser.add_argument('-h', '--redis_host', type=str, help='Hostname or IP of backing Redis', default='127.0.0.1')
+parser.add_argument('-p', --redis_port', type=str, help='Port of backing Redis', default='6379')
+parser.add_argument('-t', '--ttl', type=int, help='Cache TTL in seconds', default=60)
+parser.add_argument('-k', '--count', type=int, help='Number of items to cache', default=60)
+parser.add_argument('-w', '--pw', type=str, nargs='?')
+parser.add_argument('-T', '--test', action='store_true', help='Run end-to-end tests')
+args = parser.parse_args()
+
+def connect_backing(args): # connect to the redis instance
+    try:
+        r = redis.Redis(host=args.redis_host, port=args.redis_port, db=0, password=args.pw)
+        print(f"Successfully connected to backing Redis")
+        return(r)
+    except redis.exceptions.AuthenticationError as e: 
+        print(f"Redis password required: {e}")
+
+def redis_test_conn(r): #ensure we are connected to the redis instance. this may be unneeded or could be merged with connect_backing
     try:
         response = r.ping()
         if response:
-            print("Connected to Redis instance")
+            print("Backing Redis ping successful")
         else:
             print("Unable to connect to Redis instance")
     except redis.exceptions.ConnectionError as e:
@@ -19,12 +36,11 @@ def redis_data_gen(r, count):
         r.set(i, i**2)
     print("Added test data to Redis instance")
 
-host = '0.0.0.0' #bind address and port for flask
 port = 9999
 app = Flask(__name__)
 @app.route('/get_data', methods = ['GET'])
 def get_data():
-    time.sleep(5)
+    #time.sleep(5)
     key = request.args.get('key')
     if not key:
         return jsonify({'error': 'no key parameter'}), 400
@@ -40,16 +56,9 @@ def get_data():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Redis caching proxy')
-    parser.add_argument('--redis_host', type=str, help='Hostname or IP of backing Redis', default='127.0.0.1')
-    parser.add_argument('--redis_port', type=str, help='Port of backing Redis', default='6379')
-    parser.add_argument('--ttl', type=int, help='Cache TTL in seconds', default=60)
-    parser.add_argument('--count', type=int, help='Number of items to cache', default=60)
-    parser.add_argument('--pw', type=str)#, nargs='?')
-    args = parser.parse_args()
-
-    r = redis.Redis(host=args.redis_host, port=args.redis_port, db=0, password=args.pw)
+    r = connect_backing(args)
     cached_data = TTLCache(maxsize=args.count, ttl=args.ttl)
-    redis_test_conn(r)
-    redis_data_gen(r, 100) #where 100 is the number of entries in the redis db
-    app.run(host=host, debug=True, port=port)
+    if args.test == True:
+        redis_test_conn(r)
+        redis_data_gen(r, 100) #where 100 is the number of entries in the redis db
+    app.run(host='0.0.0.0', debug=True, port=port)
