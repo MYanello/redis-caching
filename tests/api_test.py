4e40928306acd0499405e572b474c60a779171d4
Marcus Yanello <marcus@yanello.net>
2023-11-10 16:10:31 -0600

    api test working

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,22 +1,25 @@
+from multiprocessing import Process
 from src import app
 import pytest
 import argparse
 import logging
 import requests
 import uvicorn
+import time
+import json
 
 @pytest.fixture
 def server():
-    server = uvicorn.run(app, host='0.0.0.0', port='9999')
-    with server.run_in_thread():
-        yield server
-
-def test_api():
     args = argparse.Namespace(proxy_host = '0.0.0.0', proxy_port = '9999', redis_host = '127.0.0.1', redis_port = '6379', password = 'rescale', size = 100, ttl = 100)
     application = app.redis_proxy(args)
     application.redis_data_gen(1000)
-    application.launch_server()
-    #assert True
-    response = requests.get('http://localhost:9999/get_data?key=1')
-    assert response['data'] == 1
+    proc = Process(target=application.launch_server, args=(), daemon=True)
+    proc.start()
+    time.sleep(1)
+    yield 
+    proc.kill()
     application.clean()
+
+def test_api(server):
+    response = requests.get('http://localhost:9999/get_data?key=1')
+    assert response.json()['data'] == "1"
